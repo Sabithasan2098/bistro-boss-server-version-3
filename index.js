95d62026ca3c967da6d602d34b79ac1fc27b21fd
Sabit hasan <sabithasan2098@gmail.com>
2024-08-20 09:06:41 +0600

    delete a cart item

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -62,6 +63,14 @@ async function run() {
       res.send(result);
     });
     // --------------------
+    // delete cart data by id----------
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollections.deleteOne(query);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
